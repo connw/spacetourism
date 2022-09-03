dd87c56d54e8301bc9b8e75db9c9d0d5894a6171
constance-w <58828879+constance-w@users.noreply.github.com>
2022-09-02 21:45:07 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const destinations = {
-    header: ["moon", "mars", "europa", "titan"],
+const destination = {
+    name: ["moon", "mars", "europa", "titan"],
     description: [
         "See our planet as you’ve never seen it before. A perfect relaxing trip away to help regain perspective and come back refreshed. While you’re there, take in some history by visiting the Luna 2 and Apollo 11 landing sites.",
         "Don’t forget to pack your hiking boots. You’ll need them to tackle Olympus Mons, the tallest planetary mountain in our solar system. It’s two and a half times the size of Everest!",
@@ -8,8 +8,8 @@ const destinations = {
     ],
     distance: ["384,400 km", "225 mil. km", "628 mil. km", "1.6 bil. km"],
     time: ["3 days", "9 months", "3 years", "7 years"]
-}
-const crews = {
+};
+const crew = {
     title: [
         ["Commander", "commander"],
         ["Mission Specialist", "specialist"],
@@ -28,9 +28,9 @@ const crews = {
         "Pilot on the first operational flight of the SpaceX Crew Dragon to the International Space Station. Glover is a commander in the U.S. Navy where he pilots an F/A-18. He was a crew member of Expedition 64, and served as a station systems flight engineer.",
         "Anousheh Ansari is an Iranian American engineer and co-founder of Prodea Systems. Ansari was the fourth self-funded space tourist, the first self-funded woman to fly to the ISS, and the first Iranian in space."
     ]
-}
-const technologies = {
-    term: [
+};
+const technology = {
+    name: [
         ["Launch Vehicle", "launch-vehicle"],
         ["Spaceport", "spaceport"],
         ["Space capsule", "space-capsule"]
@@ -40,79 +40,84 @@ const technologies = {
         "A spaceport or cosmodrome is a site for launching (or receiving) spacecraft, by analogy to the seaport for ships or airport for aircraft. Based in the famous Cape Canaveral, our spaceport is ideally situated to take advantage of the Earth’s rotation for launch.",
         "A space capsule is an often-crewed spacecraft that uses a blunt-body reentry capsule to reenter the Earth's atmosphere without wings. Our capsule is where you'll spend your time during the flight. It includes a space gym, cinema, and plenty of other activities to keep you entertained."
     ]
-}
-const contentNav = document.querySelectorAll(".content-nav-item");
-const image = document.querySelector("main img");
+};
+const contentNav = document.querySelectorAll(".content-nav li");
 const images = document.querySelectorAll("main img");
 
-document.querySelector(".hamb").addEventListener("click", openNav);
-document.querySelectorAll(".hamb-close, main, .mobile").forEach(elem => elem.addEventListener("click", closeNav));
+document.querySelector(".hamburger-icon").addEventListener("click", mobileNav);
 
-contentNav.forEach(elem => {
-    page = elem.parentNode.classList[1];
+contentNav.forEach(el => {
+    const page = el.parentNode.classList[1];
 
-    if (page == "destinations") { elem.addEventListener("click", destinationContent); } 
-    else if (page == "crew") { elem.addEventListener("click", crewContent); } 
-    else if (page == "technology") { elem.addEventListener("click", technologyContent); }
+    if (page == "js-destination") { 
+        el.addEventListener("click", changeDestintaion); 
+    } else if (page == "js-crew") { 
+        el.addEventListener("click", changeCrew); 
+    } else if (page == "js-technology") { 
+        el.addEventListener("click", changeTechnology); 
+    }
 });
 
-function openNav() {
-    document.querySelector(".header-nav-hamb").classList.add("show");
+function mobileNav() {
+    const hamburgerNav = document.querySelector(".site-nav--mobile");
+
+    hamburgerNav.classList.add("js-active");
+
+    document.querySelectorAll(".site-nav__close--mobile, main")
+        .forEach(el => el.addEventListener("click", () => { hamburgerNav.classList.remove("js-active"); })
+    );
 }
 
-function closeNav() {
-    document.querySelector(".header-nav-hamb").classList.remove("show");
+function activeSelection(selection) {
+    contentNav.forEach(el => { if (el.classList.contains("selected")) el.classList.remove("selected") });
+    contentNav.forEach(el => { if (el.value == selection.value) el.classList.add("selected") });
 }
 
-function destinationContent() {
+function changeDestintaion() {
     const selection = this.value;
+    const name = destination.name[selection];
+    const description = destination.description[selection];
+    const distance = destination.distance[selection];
+    const time = destination.time[selection];
+
+    activeSelection(this);
+    
+    images.forEach(el => el.setAttribute("src", "assets/destination/image-" + name + ".png"));
+    images.forEach(el => el.setAttribute("alt", name + " photo"));
+
+    document.querySelectorAll(".name").forEach(el => el.innerHTML = name);
+    document.querySelectorAll(".description").forEach(el => el.innerHTML = description);
+    document.querySelectorAll(".facts__distance .facts__value").forEach(el => el.innerHTML = distance);
+    document.querySelectorAll(".facts__time .facts__value").forEach(el => el.innerHTML = time);
+}
+
+function changeCrew() {
+    const selection = this.value;
+    const name = crew.name[selection];
+    const title = crew.title[selection];
+    const description = crew.description[selection];
 
     activeSelection(this);
 
-    image.setAttribute("src", "assets/destination/image-" + destinations.header[selection] + ".png");
-    image.setAttribute("alt", "Destination " + destinations.header[selection]);
+    images.forEach(el => el.setAttribute("src", "assets/crew/image-" + name[1] + ".png"));
+    images.forEach(el => el.setAttribute("alt", name[0] + " photo"));
 
-    document.querySelector(".content-header").innerHTML = destinations.header[selection];
-    document.querySelector(".content-desc").innerHTML = destinations.description[selection];
-    document.querySelector(".distance .content-fact-value").innerHTML = destinations.distance[selection];
-    document.querySelector(".time .content-fact-value").innerHTML = destinations.time[selection];
+    document.querySelectorAll(".title").forEach(el => el.innerHTML = title[0]);
+    document.querySelectorAll(".name").forEach(el => el.innerHTML = name[0]);
+    document.querySelectorAll(".description").forEach(el => el.innerHTML = description);
 }
 
-function crewContent() {
+function changeTechnology() {
     const selection = this.value;
+    const name = technology.name[selection];
+    const description = technology.description[selection];
 
     activeSelection(this);
 
-    images.forEach(elem => elem.setAttribute("src", "assets/crew/image-" + crews.name[selection][1] + ".png"))
-    images.forEach(elem => elem.setAttribute("alt", crews.name[selection][0] + " photo"));
+    document.querySelector(".desktop img").setAttribute("src", "assets/technology/image-" + name[1] + "-portrait.jpg");
+    document.querySelector(".mobile img").setAttribute("src", "assets/technology/image-" + name[1] + "-landscape.jpg");
+    document.querySelectorAll("main img").forEach(el => el.setAttribute("alt", name[0] + " photo"));
 
-    document.querySelectorAll(".content-crew-title").forEach(elem => elem.innerHTML = crews.title[selection][0]);
-    document.querySelectorAll(".content-crew-name").forEach(elem => elem.innerHTML = crews.name[selection][0]);
-    document.querySelectorAll(".content-desc").forEach(elem => elem.innerHTML = crews.description[selection]);
-}
-
-function technologyContent() {
-    const selection = this.value;
-
-    activeSelection(this);
-
-    document.querySelector(".desktop img").setAttribute("src", "assets/technology/image-" + technologies.term[selection][1] + "-portrait.jpg");
-    document.querySelector(".mobile img").setAttribute("src", "assets/technology/image-" + technologies.term[selection][1] + "-landscape.jpg");
-    document.querySelectorAll(".technology img").forEach(elem => elem.setAttribute("alt", technologies.term[selection][0] + " photo"));
-
-    document.querySelectorAll(".content-term").forEach(elem => elem.innerHTML = technologies.term[selection][0]);
-    document.querySelectorAll(".content-desc").forEach(elem => elem.innerHTML = technologies.description[selection]);
-}
-
-
-function activeSelection(choice) {
-    const hasMultipleDisplays = document.getElementsByTagName("main").length > 1;
-
-    contentNav.forEach(elem => { if (elem.classList.contains("selected")) elem.classList.remove("selected") });
-
-    choice.classList.add("selected");
-
-    if (hasMultipleDisplays) {
-        contentNav.forEach(elem => { if (elem.value == choice.value) elem.classList.add("selected") });
-    }
+    document.querySelectorAll(".name").forEach(el => el.innerHTML = name[0]);
+    document.querySelectorAll(".description").forEach(el => el.innerHTML = description);
 }
